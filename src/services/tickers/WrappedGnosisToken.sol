5b3c79b589da3b4652db5f74f2b6e92e637d05fb
0xAtum <0xAtum@protonmail.com>
2024-10-21 10:56:32 -0400

    Issue 34

--- src/services/tickers/WrappedGnosisToken.sol
+++ src/services/tickers/WrappedGnosisToken.sol
@@ -30,7 +30,6 @@ contract WrappedGnosisToken is ERC20, OApp {
   error CannotWrapOnMainnet();
   error CannotUnwrapOnMainnet();
   error GasLimitCannotBeZero();
-  error SlippageExceeded(uint256 amountOrIdReceiving, uint256 minAmountOut);
 
   constructor(address _owner, address _lzEndpoint, address _genesisToken)
     ERC20("WrappedGenesisToken", "WGT")
@@ -46,17 +45,13 @@ contract WrappedGnosisToken is ERC20, OApp {
     emit NewPoolAttached(_pool);
   }
 
-  function send(uint32 _dstEid, address _to, uint256 _amountIn, uint256 _minAmountOut)
+  function send(uint32 _dstEid, address _to, uint256 _amountIn)
     external
     payable
     returns (MessagingReceipt memory msgReceipt)
   {
     bytes memory option = defaultLzOption;
-    uint256 amountReceiving = _debit(_amountIn, _minAmountOut);
-
-    if (amountReceiving < _minAmountOut) {
-      revert SlippageExceeded(amountReceiving, _minAmountOut);
-    }
+    uint256 amountReceiving = _debit(_amountIn);
 
     bytes memory payload = _generateMessage(_to, amountReceiving);
     MessagingFee memory fee = _estimateFee(_dstEid, payload, option);
@@ -136,7 +131,7 @@ contract WrappedGnosisToken is ERC20, OApp {
     return _value;
   }
 
-  function _debit(uint256 _amountIn, uint256) internal returns (uint256 amountReceiving_) {
+  function _debit(uint256 _amountIn) internal returns (uint256 amountReceiving_) {
     _burn(msg.sender, _amountIn);
     return _amountIn;
   }
