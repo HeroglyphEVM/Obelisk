575cb451eb942ebd3cd9f4b488ea1dce28f45e42
0xAtum <0xAtum@protonmail.com>
2024-11-24 10:53:01 -0500

    ...

--- script/deploy/1_WrapGenesisDeploy.s.sol
+++ script/deploy/1_WrapGenesisDeploy.s.sol
@@ -3,8 +3,32 @@ pragma solidity ^0.8.0;
 
 import "../BaseScript.sol";
 import { WrappedGenesisToken } from "src/services/tickers/WrappedGenesisToken.sol";
+import "@layerzerolabs/lz-evm-protocol-v2/contracts/interfaces/IMessageLibManager.sol";
 
 contract WrapGenesisDeploy is BaseScript {
+  struct ExecutorConfig {
+    uint32 maxMessageSize;
+    address executor;
+  }
+
+  struct UlnConfig {
+    uint64 confirmations;
+    // we store the length of required DVNs and optional DVNs instead of using DVN.length
+    // directly to save gas
+    uint8 requiredDVNCount; // 0 indicate DEFAULT, NIL_DVN_COUNT indicate NONE (to
+      // override the value of default)
+    uint8 optionalDVNCount; // 0 indicate DEFAULT, NIL_DVN_COUNT indicate NONE (to
+      // override the value of default)
+    uint8 optionalDVNThreshold; // (0, optionalDVNCount]
+    address[] requiredDVNs; // no duplicates. sorted an an ascending order. allowed
+      // overlap with optionalDVNs
+    address[] optionalDVNs; // no duplicates. sorted an an ascending order. allowed
+      // overlap with requiredDVNs
+  }
+
+  uint32 internal constant CONFIG_TYPE_EXECUTOR = 1;
+  uint32 internal constant CONFIG_TYPE_ULN = 2;
+
   struct Config {
     address owner;
     address treasury;
@@ -37,7 +61,7 @@ contract WrapGenesisDeploy is BaseScript {
   uint88 private PREFIX_ID = 9970;
 
   function run() external override {
-    _loadContracts(false);
+    _loadContracts(true);
     deployerWallet = _getDeployerAddress();
 
     uint32 ETHEREUM_ID = 30_101;
@@ -50,6 +74,7 @@ contract WrapGenesisDeploy is BaseScript {
     uint88 idIndex = 0;
     string memory name;
     string memory symbol;
+    address realGenesisToken;
     for (uint88 i = 0; i < wrapConfig.length; ++i) {
       currentWrapConfig = wrapConfig[i];
 
@@ -57,8 +82,11 @@ contract WrapGenesisDeploy is BaseScript {
         idIndex++;
 
         if (block.chainid != 1 && block.chainid != currentWrapConfig.chainId) continue;
+
+        realGenesisToken = currentWrapConfig.genesisTokens[x];
         name = string.concat("Wrapped ", currentWrapConfig.genesisNames[x]);
         symbol = string.concat("W", currentWrapConfig.genesisNames[x]);
+
         (wrap, isAlreadyExisting) = _tryDeployContractDeterministic(
           name,
           _generateSeed(PREFIX_ID + idIndex),
@@ -69,7 +97,7 @@ contract WrapGenesisDeploy is BaseScript {
             symbol,
             currentWrapConfig.lzEID,
             currentWrapConfig.lzEndpoint,
-            currentWrapConfig.genesisTokens[x]
+            realGenesisToken
           )
         );
 
@@ -81,19 +109,26 @@ contract WrapGenesisDeploy is BaseScript {
           );
         }
 
-        uint256 sending = WrappedGenesisToken(currentWrapConfig.genesisTokens[x])
-          .balanceOf(deployerWallet) / 2;
-
-        require(sending > 0, "No tokens to send");
-
-        vm.broadcast(_getDeployerPrivateKey());
-        WrappedGenesisToken(currentWrapConfig.genesisTokens[x]).approve(
-          address(wrap), sending
-        );
-
-        vm.broadcast(_getDeployerPrivateKey());
-        WrappedGenesisToken(wrap).addRewardOnMainnet(sending);
+        sendToPool(ETHEREUM_ID, wrap, realGenesisToken);
       }
     }
+  }
+
+  function sendToPool(uint32 ETHEREUM_ID, address wrap, address realGenesisToken)
+    internal
+  {
+    uint256 balance = WrappedGenesisToken(realGenesisToken).balanceOf(deployerWallet);
+    uint256 sending = balance / 2;
+    uint256 fee = WrappedGenesisToken(wrap).estimateFee(ETHEREUM_ID, wrap, sending);
+
+    if (WrappedGenesisToken(realGenesisToken).allowance(deployerWallet, wrap) == 0) {
+      vm.broadcast(_getDeployerPrivateKey());
+      WrappedGenesisToken(realGenesisToken).approve(wrap, sending);
+    }
+
+    console.log(WrappedGenesisToken(realGenesisToken).name(), balance - sending);
+
+    vm.broadcast(_getDeployerPrivateKey());
+    WrappedGenesisToken(wrap).addRewardOnMainnet{ value: fee }(sending);
   }
 }
